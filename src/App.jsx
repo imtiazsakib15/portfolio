73e784feb5390a489d47b87693ec8b18c40b33f0
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-08-02 01:06:40 +0600

    click spark animation added

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,27 @@ import Navbar from "./components/Navbar/Navbar";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import AboutMe from "./components/AboutMe/AboutMe";
+import ClickSpark from "./components/blocks/Animations/ClickSpark/ClickSpark";
 
 function App() {
   return (
-    <div className="bg-[#003566ff] text-white">
-      <Navbar />
-      <Banner />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-      <Toaster position="top-center" reverseOrder={false} />
+    <div className="bg-[#003566ff] text-white relative">
+      <ClickSpark
+        sparkColor="#fff"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={500}
+      >
+        <Navbar />
+        <Banner />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+        <Toaster position="top-center" reverseOrder={false} />
+      </ClickSpark>
     </div>
   );
 }
